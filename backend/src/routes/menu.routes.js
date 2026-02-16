1857e1da80bd5e6a019e5cf83b9ff43b4746f838
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 16:55:14 +0530

    docs(swagger): add menu API documentation

--- backend/src/routes/menu.routes.js
+++ backend/src/routes/menu.routes.js
@@ -12,16 +12,59 @@ const {
 
 
 // Public routes
+/**
+ * @swagger
+ * /api/menu/restaurant/{restaurantId}:
+ *   get:
+ *     summary: Get menu by restaurant
+ *     tags: [Menu]
+ */
 router.get("/restaurant/:restaurantId", getMenuByRestaurant);
 
+/**
+ * @swagger
+ * /api/menu/{id}:
+ *   get:
+ *     summary: Get menu item by ID
+ *     tags: [Menu]
+ */
 router.get("/:id", getMenuItemById);
 
 
 // Admin routes
+/**
+ * @swagger
+ * tags:
+ *   name: Menu
+ *   description: Menu management APIs
+ */
+
+
+/**
+ * @swagger
+ * /api/menu:
+ *   post:
+ *     summary: Create menu item
+ *     tags: [Menu]
+ */
 router.post("/", createMenuItem);
 
+/**
+ * @swagger
+ * /api/menu/{id}:
+ *   put:
+ *     summary: Update menu item
+ *     tags: [Menu]
+ */
 router.put("/:id", updateMenuItem);
 
+/**
+ * @swagger
+ * /api/menu/{id}:
+ *   delete:
+ *     summary: Delete menu item
+ *     tags: [Menu]
+ */
 router.delete("/:id", deleteMenuItem);
 
 
