6deac6893340caff3439a1d223e4a96e6b47ff83
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 14:41:34 +0530

    menu schema created with reference to restaurant

---
+++ backend/src/models/MenuItem.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const menuItemSchema = new mongoose.Schema(
+  {
+    restaurant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    image: {
+      type: String,
+    },
+
+    category: {
+      type: String,
+    },
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("MenuItem", menuItemSchema);
