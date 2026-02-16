d5897df6d3690909b116268d5bcd20c7a1221a3d
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 14:42:24 +0530

    cart model created

---
+++ backend/src/models/Cart.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema({
+  menuItem: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "MenuItem",
+  },
+
+  quantity: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [cartItemSchema],
+
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
