3702ff161066d14c1f921097fff38bcb5d164eca
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 16:56:59 +0530

    docs(swagger): add cart API documentation

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -10,15 +10,57 @@ const {
   clearCart,
 } = require("../controllers/cart.controller");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Cart
+ *   description: Cart management APIs
+ */
 
+
+/**
+ * @swagger
+ * /api/cart/add:
+ *   post:
+ *     summary: Add item to cart
+ *     tags: [Cart]
+ */
 router.post("/add", addToCart);
 
+/**
+ * @swagger
+ * /api/cart/{userId}:
+ *   get:
+ *     summary: Get user cart
+ *     tags: [Cart]
+ */
 router.get("/:userId", getCart);
 
+/**
+ * @swagger
+ * /api/cart/update:
+ *   put:
+ *     summary: Update cart item
+ *     tags: [Cart]
+ */
 router.put("/update", updateCartItem);
 
+/**
+ * @swagger
+ * /api/cart/remove:
+ *   delete:
+ *     summary: Remove item from cart
+ *     tags: [Cart]
+ */
 router.delete("/remove", removeFromCart);
 
+/**
+ * @swagger
+ * /api/cart/clear:
+ *   delete:
+ *     summary: Clear cart
+ *     tags: [Cart]
+ */
 router.delete("/clear", clearCart);
 
 
