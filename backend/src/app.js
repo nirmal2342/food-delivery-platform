996c27c5313a11bd15be815fce4cb0083c8f8348
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 16:07:54 +0530

    connect cart routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const restaurantRoutes = require("./routes/restaurant.routes");
 const menuRoutes = require("./routes/menu.routes");
+const cartRoutes = require("./routes/cart.routes");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/restaurants", restaurantRoutes);
 app.use("/api/menu", menuRoutes);
+app.use("/api/cart", cartRoutes);
 
 app.get("/", (req, res) => {
   res.send("Food Delivery API Running");
