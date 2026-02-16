30e2216b8d7e491ff697c51659c770df687b8439
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 15:54:22 +0530

    add menu routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
 const restaurantRoutes = require("./routes/restaurant.routes");
+const menuRoutes = require("./routes/menu.routes");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/menu", menuRoutes);
 
 app.get("/", (req, res) => {
   res.send("Food Delivery API Running");
