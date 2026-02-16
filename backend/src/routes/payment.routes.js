cbc7b0dd98b9696b12ee23842de6e43100f16668
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 17:00:31 +0530

    docs(swagger): add payment API documentation

--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -8,11 +8,39 @@ const {
   updatePaymentStatus,
 } = require("../controllers/payment.controller");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Payments
+ *   description: Payment management APIs
+ */
 
+
+/**
+ * @swagger
+ * /api/payments/create:
+ *   post:
+ *     summary: Create payment
+ *     tags: [Payments]
+ */
 router.post("/create", createPayment);
 
+/**
+ * @swagger
+ * /api/payments/order/{orderId}:
+ *   get:
+ *     summary: Get payment by order
+ *     tags: [Payments]
+ */
 router.get("/order/:orderId", getPaymentByOrder);
 
+/**
+ * @swagger
+ * /api/payments/status/{id}:
+ *   put:
+ *     summary: Update payment status
+ *     tags: [Payments]
+ */
 router.put("/status/:id", updatePaymentStatus);
 
 
