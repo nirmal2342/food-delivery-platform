bf092ef34f7a8e9119c0a4fc32abdf447d71738a
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 16:58:27 +0530

    docs(swagger): add order API documentation

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -10,15 +10,57 @@ const {
   cancelOrder,
 } = require("../controllers/order.controller");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Orders
+ *   description: Order management APIs
+ */
 
+
+/**
+ * @swagger
+ * /api/orders/place:
+ *   post:
+ *     summary: Place order
+ *     tags: [Orders]
+ */
 router.post("/place", placeOrder);
 
+/**
+ * @swagger
+ * /api/orders/user/{userId}:
+ *   get:
+ *     summary: Get user orders
+ *     tags: [Orders]
+ */
 router.get("/user/:userId", getUserOrders);
 
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   get:
+ *     summary: Get order by ID
+ *     tags: [Orders]
+ */
 router.get("/:id", getOrderById);
 
+/**
+ * @swagger
+ * /api/orders/status/{id}:
+ *   put:
+ *     summary: Update order status
+ *     tags: [Orders]
+ */
 router.put("/status/:id", updateOrderStatus);
 
+/**
+ * @swagger
+ * /api/orders/cancel/{id}:
+ *   put:
+ *     summary: Cancel order
+ *     tags: [Orders]
+ */
 router.put("/cancel/:id", cancelOrder);
 
 
