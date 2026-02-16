0b6650eb96602c1284aaa66d632bdc3572168ce1
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 17:16:50 +0530

    docs(swagger): add delivery API documentation

--- backend/src/routes/delivery.routes.js
+++ backend/src/routes/delivery.routes.js
@@ -9,13 +9,48 @@ const {
   getPartnerDeliveries,
 } = require("../controllers/delivery.controller");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Delivery
+ *   description: Delivery management APIs
+ */
 
+
+/**
+ * @swagger
+ * /api/delivery/assign:
+ *   post:
+ *     summary: Assign delivery partner
+ *     tags: [Delivery]
+ */
 router.post("/assign", assignDelivery);
 
+/**
+ * @swagger
+ * /api/delivery/order/{orderId}:
+ *   get:
+ *     summary: Track delivery
+ *     tags: [Delivery]
+ */
 router.get("/order/:orderId", getDeliveryByOrder);
 
+/**
+ * @swagger
+ * /api/delivery/status/{id}:
+ *   put:
+ *     summary: Update delivery status
+ *     tags: [Delivery]
+ */
 router.put("/status/:id", updateDeliveryStatus);
 
+/**
+ * @swagger
+ * /api/delivery/partner/{partnerId}:
+ *   get:
+ *     summary: Get partner deliveries
+ *     tags: [Delivery]
+ */
 router.get("/partner/:partnerId", getPartnerDeliveries);
 
 
