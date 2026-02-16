4d17cc02d776567665f9081b4fef96016b5fdc67
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 15:41:56 +0530

    connect restaurant routes to express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
+const restaurantRoutes = require("./routes/restaurant.routes");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/restaurants", restaurantRoutes);
 
 app.get("/", (req, res) => {
   res.send("Food Delivery API Running");
