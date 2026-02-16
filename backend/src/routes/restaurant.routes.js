faccf84a8b90f10afde8a0a00108c40ae63d8db4
Nirmal Sugandhi <nirmal.sugandhi@softude.com>
2026-02-16 16:50:17 +0530

    docs(swagger): add restaurant API documentation

--- backend/src/routes/restaurant.routes.js
+++ backend/src/routes/restaurant.routes.js
@@ -12,16 +12,62 @@ const {
 
 
 // Public routes
+/**
+ * @swagger
+ * tags:
+ *   name: Restaurants
+ *   description: Restaurant management APIs
+ */
+
+
+/**
+ * @swagger
+ * /api/restaurants:
+ *   get:
+ *     summary: Get all restaurants
+ *     tags: [Restaurants]
+ *     responses:
+ *       200:
+ *         description: List of restaurants
+ */
 router.get("/", getRestaurants);
 
+/**
+ * @swagger
+ * /api/restaurants/{id}:
+ *   get:
+ *     summary: Get restaurant by ID
+ *     tags: [Restaurants]
+ */
 router.get("/:id", getRestaurantById);
 
 
 // Admin routes
+/**
+ * @swagger
+ * /api/restaurants:
+ *   post:
+ *     summary: Create restaurant
+ *     tags: [Restaurants]
+ */
 router.post("/", createRestaurant);
 
+/**
+ * @swagger
+ * /api/restaurants/{id}:
+ *   put:
+ *     summary: Update restaurant
+ *     tags: [Restaurants]
+ */
 router.put("/:id", updateRestaurant);
 
+/**
+ * @swagger
+ * /api/restaurants/{id}:
+ *   delete:
+ *     summary: Delete restaurant
+ *     tags: [Restaurants]
+ */
 router.delete("/:id", deleteRestaurant);
 
 
